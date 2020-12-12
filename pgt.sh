4f517fdd995dbe352af2c704cb0978ddb771233f
PRINCE KUMAR <56459297+princekrvert@users.noreply.github.com>
2020-12-12 14:50:31 +0530

    Add files via upload

--- pgt.sh
+++ pgt.sh
@@ -27,11 +27,11 @@ echo -e "\e[3${ran};1m
 
 }
 banner
-
+									
 #make a menu options here
 echo -e "\e[31m(\e[34m1\e[31m) \e[33;1m Normal mode"
 echo -e "\e[31m(\e[34m2\e[31m) \e[33;1m Moderate mode"
-echo -e "\e[31m(\e[34m3\e[31m) \e[33;1m Advance mode (comming soon..)"
+echo -e "\e[31m(\e[34m3\e[31m) \e[33;1m Advance mode "
 read m_optn	       
 if [ $m_optn -eq 1 ]; then
 	echo -e "\e[95m Starting normal mode."
@@ -46,13 +46,31 @@ elif [ $m_optn -eq 2 ]; then
 		echo -e "\e[32;1m Trying to start narmal mode"
 		bash pgtn.sh
 	elif [ $yn = N ] || [ $yn = n ];then
+		
 		echo -e "\e[32;1m Exiting program.."
 	else
 		echo -e "\e[32;1m Invalid option."
 	fi	      
+elif [ $m_optn -eq 3 ];then
+	echo -e "\e[95m Starting advance mode."
+	cd .pweb
+	bash pgta.sh   			
+	echo -e "\e[37m Want to run Normal mode..[Y]/[N] "
+read yn #reading for user choise                        
+if [ $yn = Y ] || [ $yn = y ];then
+                echo -e "\e[32;1m Trying to start narmal mode "
+                bash pgtn.sh
+        elif [ $yn = N ] || [ $yn = n ];then
+
+                echo -e "\e[32;1m Exiting program.."
+        else
+                echo -e "\e[32;1m Invalid option."
+        fi
 else
 	echo "this optinon is not available"
 fi
-																	
-											                            
+																															
+																								                            
 #don't copy this script..
+					
+									
